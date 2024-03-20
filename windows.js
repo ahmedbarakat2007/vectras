177fe74d86aef03aff4e4b11f38df0b0d8d6bd8f
Ahmed Barakat <118398763+ahmedbarakat2007@users.noreply.github.com>
2024-03-20 15:03:14 +0200

    Add files via upload

--- windows.js
+++ windows.js
@@ -1351,4 +1351,30 @@ function window51(){
     }
   }
 }
+function window52(){
+  var modal = document.getElementById("myModal52");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn52");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close52")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
  

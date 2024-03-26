ade06ba5140745b20332c6eb1244221c080f3efa
Ahmed Barakat <118398763+ahmedbarakat2007@users.noreply.github.com>
2024-03-26 02:44:26 +0000

    Add files via upload

--- windows.js
+++ windows.js
@@ -1377,4 +1377,55 @@ function window52(){
     }
   }
 }
- 
+function window53(){
+  var modal = document.getElementById("myModal53");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn53");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close53")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+function window54(){
+  var modal = document.getElementById("myModal54");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn54");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close54")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
